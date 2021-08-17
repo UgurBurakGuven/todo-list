cfed90ac6aa36e79a9aa45449e0f0202a54439fb
Burak <kiwimedya@gmail.com>
2021-08-17 17:17:28 +0300

    responsive bottom

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Navbar, Container, Button } from "react-bootstrap";
+import { Navbar, Container, Button, Row, Col } from "react-bootstrap";
 import NoteCard from "../components/NoteCard";
 import localforage from "localforage";
+import Link from "next/link";
 import { error } from "next/dist/build/output/log";
 
 export default function Home() {
@@ -51,55 +52,67 @@ export default function Home() {
   return (
     <>
       <Navbar expand="lg" variant="dark" bg="dark">
-        <Container>
-          <Navbar.Brand href="#" style={{ color: "#82d7f7" }}>
-            Todo List
-          </Navbar.Brand>
-        </Container>
+        <div className={"mx-2"}>
+          <Link href={"#"} style={{ color: "#82d7f7" }}>
+            <a
+              className={"text-decoration-none text-light ms-4"}
+              style={{ fontSize: "1.5rem" }}
+            >
+              Todo List
+            </a>
+          </Link>
+        </div>
       </Navbar>
       <div>
         {notes.map((note, index) => (
           <NoteCard key={index} note={note} onDelete={onDelete} />
         ))}
       </div>
+      <div
+        className={"w-100 bg-dark bottom-0 position-fixed"}
+        style={{ height: "4rem" }}
+      >
+        <form onSubmit={addNote} className={"d-flex justify-content-end"}>
+          <Button
+            className={"position-fixed  float-end mx-sm-5"}
+            type={"submit"}
+            style={{
+              zIndex: "1",
+              lineHeight: "1rem",
+              bottom: "0.6rem",
+              width: "3rem",
+              height: "3rem",
+              borderRadius: "20%",
+              textAlign: "center",
+              fontSize: "1.7rem",
+              fontWeight: "bold",
+              color: "black",
+              background: "#82d7f7",
+              boxShadow: "0.3rem 0.3rem 0.4rem 0rem rgba(0,0,0,0.8)",
+            }}
+          >
+            +
+          </Button>
 
-      <form onSubmit={addNote}>
-        <Button
-          className={"position-fixed"}
-          type={"submit"}
-          style={{
-            right: "10%",
-            zIndex: "1",
-            lineHeight: "10px",
-            bottom: "10px",
-            width: "60px",
-            height: "40px",
-            borderRadius: "20%",
-            textAlign: "center",
-            fontSize: "28px",
-            fontWeight: "bold",
-            color: "black",
-            background: "#82d7f7",
-            boxShadow: "5px 5px 6px 0px rgba(0,0,0,0.8)",
-          }}
-        >
-          +
-        </Button>
-        <input
-          placeholder={"Enter Notes"}
-          type="text"
-          className={
-            "bottom-0 position-fixed w-100 p-1 btn-outline- border-0 bg-dark"
-          }
-          value={text}
-          onChange={(e) => updateText(e)}
-          style={{
-            color: "#f1f1f1",
-            fontSize: "16px",
-            height: "56px",
-          }}
-        />
-      </form>
+          <input
+            placeholder={"Enter Notes"}
+            type="text"
+            className={
+              "bottom-0 position-fixed  w-75 p-1 btn-outline- border-0 "
+            }
+            value={text}
+            onChange={(e) => updateText(e)}
+            style={{
+              color: "#f1f1f1",
+              fontSize: "1rem",
+              marginBottom: "0.3rem",
+              height: "3.4rem",
+              left: "2rem",
+              backgroundColor: " #3c3c3c",
+            }}
+          />
+        </form>
+      </div>
     </>
   );
 }
