a9bdb9fc5510ef61d42bb92322b5d2be59494c49
Burak <kiwimedya@gmail.com>
2021-08-17 15:52:12 +0300

    NoteCard

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { BiTrash } from "react-icons/bi";
 
 function NoteCard(props) {
   return (
-    <Card className={" d-flex col-md-11 p-2  m-5"}>
-      <Card.Header as="h5">NOT</Card.Header>
+    <Card className={" d-flex col-md-11 p-2 mx-5 my-3"}>
+      <Card.Header as="h5">
+        NOT{" "}
+        <BiTrash
+          onClick={() => props.onDelete(props.note.id)}
+          className={"d-flex float-end"}
+          style={{ cursor: "pointer" }}
+        />
+      </Card.Header>
       <Card.Body>
-        <Card.Text>{props.text}</Card.Text>
+        <Card.Text>{props.note.text}</Card.Text>
       </Card.Body>
     </Card>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Navbar, Container, Button } from "react-bootstrap";
 import NoteCard from "../components/NoteCard";
 import localforage from "localforage";
+import { error } from "next/dist/build/output/log";
 
 export default function Home() {
   const [text, setText] = useState("");
@@ -14,29 +15,34 @@ export default function Home() {
     });
   }, []);
 
+  function onDelete(id) {
+    localforage.getItem("notes", function (err, lsNotes) {
+      const note = lsNotes.find((o) => o.id === id);
+      const index = lsNotes.indexOf(note);
+      if (index > -1) {
+        lsNotes.splice(index, 1);
+      }
+      localforage.setItem("notes", lsNotes).then(() => setNotes(lsNotes));
+    });
+  }
   const addNote = () => {
     if (text === "") {
       return;
     }
-    localforage
-      .setItem("notes", [text])
-      .then(() => {})
-      .catch((err) => {
-        console.log(err);
-      });
 
     localforage.getItem("notes", function (err, notes) {
       notes = notes || [];
-      notes.push(text);
+      notes.push({
+        text,
+        id: new Date().getTime(),
+      });
       setNotes(notes);
       localforage.setItem("notes", notes).then(() => {
         return localforage.getItem("notes", function (err, notes) {
-          console.log(notes);
+          setText("");
         });
       });
     });
-
-    setText("");
   };
   const updateText = (e) => {
     setText(e.target.value);
@@ -46,48 +52,54 @@ export default function Home() {
     <>
       <Navbar expand="lg" variant="dark" bg="dark">
         <Container>
-          <Navbar.Brand href="#">Todo List</Navbar.Brand>
+          <Navbar.Brand href="#" style={{ color: "#82d7f7" }}>
+            Todo List
+          </Navbar.Brand>
         </Container>
       </Navbar>
       <div>
-        {notes.map((val, key) => (
-          <NoteCard key={key} text={val} />
+        {notes.map((note, index) => (
+          <NoteCard key={index} note={note} onDelete={onDelete} />
         ))}
       </div>
-      <Button
-        className={"position-fixed"}
-        style={{
-          right: "10%",
-          zIndex: "1",
-          lineHeight: "2%",
-          bottom: "2%",
-          width: "5%",
-          height: "5%",
-          borderRadius: "20%",
-          textAlign: "center",
-          fontSize: "28px",
-          fontWeight: "bold",
-          color: "#f1f1f1",
-          background: "#595a5c",
-          boxShadow: "5px 5px 6px 0px rgba(0,0,0,0.8)",
-        }}
-        onClick={addNote}
-      >
-        +
-      </Button>
-      <input
-        placeholder={"Enter Notes"}
-        type="text"
-        className={"bottom-0 position-fixed w-100 p-1 btn-outline- border-0"}
-        value={text.text}
-        onChange={(e) => updateText(e)}
-        style={{
-          color: "#f1f1f1",
-          fontSize: "16px",
-          backgroundColor: "#595a5c",
-          height: "80px",
-        }}
-      />
+
+      <form onSubmit={addNote}>
+        <Button
+          className={"position-fixed"}
+          type={"submit"}
+          style={{
+            right: "10%",
+            zIndex: "1",
+            lineHeight: "2%",
+            bottom: "2%",
+            width: "5%",
+            height: "5%",
+            borderRadius: "20%",
+            textAlign: "center",
+            fontSize: "28px",
+            fontWeight: "bold",
+            color: "black",
+            background: "#82d7f7",
+            boxShadow: "5px 5px 6px 0px rgba(0,0,0,0.8)",
+          }}
+        >
+          +
+        </Button>
+        <input
+          placeholder={"Enter Notes"}
+          type="text"
+          className={
+            "bottom-0 position-fixed w-100 p-1 btn-outline- border-0 bg-dark"
+          }
+          value={text}
+          onChange={(e) => updateText(e)}
+          style={{
+            color: "#f1f1f1",
+            fontSize: "16px",
+            height: "56px",
+          }}
+        />
+      </form>
     </>
   );
 }
